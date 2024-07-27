e95a811505d1d83da86b7940ac77c7480cd2d2a1
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-07-27 21:34:20 +0900

    Vue testing

---
+++ pages/btntest/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+function decrement() {
+  count.value--
+}
+
+
+
+</script>
+
+<template>
+    <div>
+    <button @click="increment">Count is: {{ count }}</button>
+    <button @click="decrement">Count is: {{ count }}</button>
+    <div>Count is: {{ count }}</div>
+
+    </div>
+
+</template> 
---
+++ pages/timer/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+window.addEventListener('load', function () {
+  console.warn('LOADED');
+});
+
+</script>
+
+<template>
+  <div>
+    <h2>タイマーテスト</h2>
+      <p>TIME: {{ count }} </p>
+  </div>
+</template>
