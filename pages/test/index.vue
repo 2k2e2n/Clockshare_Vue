8c6f9aa6dab6cc243cdfb1e5f6b743e2e5c74574
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-07-27 17:19:39 +0900

    Create index.vue

---
+++ pages/test/index.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    <h2>簡単メモ帳</h2>
+      <label>お名前</label>
+      <span>お名前を入力してください</span>
+      <div>
+        <button type="submit">利用する</button>
+      </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+};
+</script>
