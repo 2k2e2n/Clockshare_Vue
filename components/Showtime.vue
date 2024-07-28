c1d007c99704239a5ebc9242af120d45733012be
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-07-28 23:08:29 +0900

    commit

--- components/Showtime.vue
+++ components/Showtime.vue
@@ -1,31 +1,47 @@
 <script setup>
-import { watch } from 'vue'
+import { ref, watch, defineProps } from 'vue'
 
 //親コンポーネントから受け取るデータ指定
-defineProps(['time']);
-let timeS = ref(0);
-let timeM = ref(0);
-let timeH = ref(0);
+interface Props {
+  parentToChildProp: Number
+}
+const props = defineProps<Props>()
 
-watch(
-  //timeの値を監視し、newtimeに変更後の値、oldtimeに変更前の値
-  time,
-  (newtime, oldtime) => {
-  timeS.value = padTime(time.value % 60);
-  timeM.value = padTime(Math.floor(time.value / 60) % 60);
-  timeH.value = padTime(Math.floor(time.value / (60 * 60)) % 24);
-  } 
-);
-
-// 0:4:20 -> 00:04:20 桁数をあわせる
-function padTime(value) {
-  return value.toString().padStart(2, '0');
+      const timeS = ref(0);
+      const timeM = ref(0);
+      const timeH = ref(0);
+/*
+const intervalId = window.setInterval(loop, 1000);  // 1秒に1回実行
+function loop() {
+  console.log(time.value);
 }
 
+
+      watch(
+        //timeの値を監視し、newtimeに変更後の値、oldtimeに変更前の値
+        time,
+        (newtime, oldvalue) => {
+        timeS.value = padTime(time.value % 60);
+        timeM.value = padTime(Math.floor(time.value / 60) % 60);
+        timeH.value = padTime(Math.floor(time.value / (60 * 60)) % 24);
+        console.log(time);
+        } 
+      );
+
+
+
+
+
+      // 0:4:20 -> 00:04:20 桁数をあわせる
+      function padTime(value) {
+        return value.toString().padStart(2, '0');
+      }
+*/
 </script>
 <template>
   <div>
     <h2>タイマーテスト</h2>
-    <h3>{{ timeH }}:{{ timeM }}:{{ timeS }}</h3>
+
+    <h3>{{ timeH.value }}:{{ timeM.value }}:{{ timeS.value }}</h3>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,36 @@
-<template>
-  <Tutorial/>
-</template>
+<script setup>
+import { ref } from 'vue';
+import Showtime from '@/components/Showtime.vue';
 
-<script lang="ts">
-import Vue from 'vue'
+const time  = ref(0);
+const timeS = ref(0);
+const timeM = ref(0);
+const timeH = ref(0);
+const isRest = ref(false);
 
-export default Vue.extend({
-  name: 'IndexPage'
-})
+let timer = 0;
+
+window.addEventListener('load', function () {
+  time.value=0;
+  console.warn('LOADED');
+  const intervalId = window.setInterval(countup, 1000);  // 1秒に1回実行
+});
+
+function countup() {
+  if(!isRest.value) {
+    time.value++;
+  }
+}
+
+function toggleRest() {
+  isRest.value = !isRest.value;
+}
 </script>
+<template>
+  <div>
+    <h2>タイマーテスト</h2>
+    <button @click="toggleRest">{{ isRest ? 'REST(True)' : 'TIME(False)' }}</button>
+    <p>TIME: {{ time }} </p>
+    <Showtime :settime='timer.value' />
+  </div>
+</template>
