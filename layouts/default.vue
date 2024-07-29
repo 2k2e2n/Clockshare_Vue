ea8b85fc145550d37321b6f637424f4fd56d6ca8
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-07-30 01:41:49 +0900

    add Header link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <p class="header">Header
-      <router-link to="/">Top</router-link>
-      <router-link to="/todo">Todo</router-link>
+    <p class="header">Header　　　
+      <router-link class="header-text" to="/">Top　</router-link>
+      <router-link class="header-text" to="/todo">Todo　</router-link>
     </p>
     <nuxt />
     <p>Footer</p>
@@ -12,5 +12,9 @@
 <style>
 .header {
   background-color: aqua;
+  display: flex;
+
+}
+.header-text {
 }
 </style>
