64c4f2fbeed454272b0e9b4ea11416ae1555fddf
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-08-12 22:01:26 +0900

    add background

--- pages/test.vue
+++ pages/test.vue
@@ -9,10 +9,21 @@ window.addEventListener('load', function () {
 </script>
 
 <template>
-  <div>
-    <h2>TESTページ</h2>
-    <h1>画像のテスト</h1>
-    <p> 画像を指定するには、assets/imagesに画像を配置し、imgタグで指定</p>
-    <img src="@/assets/images/image.webp">
-  </div>
+<div  class="bg">
+  <h2 class="text">TESTページ </h2>
+  <h1 class="text">画像のテスト</h1>
+  <p  class="text"> 画像を指定するには、assets/imagesに画像を配置し、imgタグで指定</p>
+  <img src="@/assets/images/image.webp">
+</div>
 </template>
+
+<style scoped>
+.text {
+  color: white;
+}
+.bg {
+  background-image: url("~@/assets/images/background.webp");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
