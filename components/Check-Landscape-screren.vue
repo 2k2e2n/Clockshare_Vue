0f8bf00dc780130a3a5c6fa3e6a4b9ce8f085fa0
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-08-24 16:58:56 +0900

    Formatted text(Use prettier)

--- components/Check-Landscape-screren.vue
+++ components/Check-Landscape-screren.vue
@@ -1,41 +1,40 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 
 onMounted(() => {
-check_Landscape_Sceen();
+  check_Landscape_Sceen();
 });
-
 
 function check_Landscape_Sceen() {
   console.warn("画面の傾きをチェック！");
-  
 }
 </script>
 
 <template>
-  <div >
-
+  <div>
     <div class="overlay">
-      <img  class="landscape-screen" src="@/assets/images/landscape-screen.gif">
+      <img
+        class="landscape-screen"
+        src="@/assets/images/landscape-screen.gif"
+      />
     </div>
   </div>
 </template>
 
 <style scoped>
-
 @media screen and (orientation: portrait) {
   /* 縦向きの場合のスタイル */
   .landscape-screen {
-  width: 40vh;
-  z-index: 1001; /* 他の要素の上に表示 */
-  transition: background-color 0.3s ease;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+    width: 40vh;
+    z-index: 1001; /* 他の要素の上に表示 */
+    transition: background-color 0.3s ease;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
   .overlay {
-  position: fixed;
+    position: fixed;
     top: 0;
     left: 0;
     width: 100%;
@@ -46,17 +45,17 @@ function check_Landscape_Sceen() {
     align-items: center;
     z-index: 1000; /* 他の要素の上に表示 */
     transition: background-color 0.3s ease;
-  } 
-} 
+  }
+}
 @media only screen and (orientation: landscape) {
   .landscape-screen {
-  width: 400px;
-  z-index: -1001; /* 他の要素の上に表示 */
-  transition: background-color 0.3s ease;
-  display: none;
+    width: 400px;
+    z-index: -1001; /* 他の要素の上に表示 */
+    transition: background-color 0.3s ease;
+    display: none;
   }
   .overlay {
-  position: fixed;
+    position: fixed;
     top: 0;
     left: 0;
     width: 100%;
@@ -68,7 +67,5 @@ function check_Landscape_Sceen() {
     z-index: -1000; /* 他の要素の上に表示 */
     transition: background-color 0.3s ease;
   }
-} 
-
-
+}
 </style>
